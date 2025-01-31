fef4999136c56099749070cabd1bdc2030f94f66
Tycoon <tycoon@Tycoons-MacBook-Pro.local>
2025-01-31 15:37:58 +0100

    Changes made app done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useMemo } from "react";
 
 import "./App.css";
 import ButtonAdd from "./ButtonAdd";
--- src/Task.jsx
+++ src/Task.jsx
@@ -1,12 +1,17 @@
-import { useEffect } from "react";
-
-const Task = ({ task, setTask }) => {
-  const deleteItem = (theTask) => {
-    const filteredList = task.filter((element, index) => {
-      return index != theTask;
-    });
-    setTask(filteredList);
-  };
+import React from "react";
+import { useCallback } from "react";
+const Task = React.memo(({ task, setTask }) => {
+  console.log("Task component renders");
+  const deleteItem = useCallback(
+    (theTask) => {
+      console.log("Delete Mounted");
+      const filteredList = task.filter((element, index) => {
+        return index != theTask;
+      });
+      setTask(filteredList);
+    },
+    [setTask]
+  );
 
   return (
     <ol>
@@ -20,5 +25,5 @@ const Task = ({ task, setTask }) => {
       ))}
     </ol>
   );
-};
+});
 export default Task;
